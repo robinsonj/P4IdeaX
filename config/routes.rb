27e1b83d06c554b185a4247f42254b8bae57fa06
robinsonj <robinsonjosephlewis@gmail.com>
2014-02-28 17:27:38 -0800

    Generated base User scaffold.
    No changes to default templates were made.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 P4IDX::Application.routes.draw do
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
