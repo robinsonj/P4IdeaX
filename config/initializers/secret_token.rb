49e4720c5229b4de6828fad522175b0588141754
robinsonj <robinsonjosephlewis@gmail.com>
2014-03-03 08:44:56 -0800

    Added secret token generation for encrypting user cookies.
    If token file does not exist, a new one is generated.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -7,6 +7,19 @@
 # no regular words or you'll be exposed to dictionary attacks.
 # You can use `rake secret` to generate a secure secret key.
 
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret_token')
+  if File.exists?(token_file)
+    File.read(token_file).chomp
+  else
+    new_token = SecureRandom.hex(64)
+    File.write(token_file, new_token)
+    new_token
+  end
+end
+
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-P4IDX::Application.config.secret_key_base = 'b3fa5228021ee6843365c3b771a3161c0f0470ed80a88f8ae2f23924a0a10d43d9ade9e2c19fa75b335a1ff398652fc1deb3fec2b6c4717ed535ef130276e7eb'
+P4IDX::Application.config.secret_key_base = secure_token
