30dbb792eecb2244a5f490d95b8e8bc6c745b5e9
robinsonj <robinsonjosephlewis@gmail.com>
2014-03-03 14:38:19 -0800

    Set default and enforceable locales for I18n.

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,11 @@ module P4IDX
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :en
+
+    # Enforce available locales for I18n.
+    config.i18n.enforce_available_locales = true
+    I18n.config.enforce_available_locales = true
+
   end
 end
