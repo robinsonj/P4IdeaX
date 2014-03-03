da9695ea567f37ff4243901856d23b6d9688eff2
robinsonj <robinsonjosephlewis@gmail.com>
2014-03-03 15:01:41 -0800

    Removed non-conforming syntax from HomeRequests tests status messages.

--- spec/requests/home_pages_spec.rb
+++ spec/requests/home_pages_spec.rb
@@ -2,21 +2,21 @@ require 'spec_helper'
 
 describe "HomeRequests" do
   describe "GET index" do
-    it "renders home#index template." do
+    it "renders home#index template" do
       get home_index_path
       response.status.should be(200)
     end
   end
 
   describe "GET about" do
-    it "renders home#about template." do
+    it "renders home#about template" do
       get home_about_path
       response.status.should be(200)
     end
   end
 
   describe "GET contact" do
-    it "renders home#contact template." do
+    it "renders home#contact template" do
       get home_contact_path
       response.status.should be(200)
     end
