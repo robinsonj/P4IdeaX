e4bd4061fae9ac6b7754327aefe90026cd03c019
robinsonj <robinsonjosephlewis@gmail.com>
2014-03-03 09:04:47 -0800

    Cleaned up application.rb after switch to rspec.

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 
-# require 'rails/all'
-
 # Require individual railtie frameworks, excluding rails/test_unit
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "sprockets/railtie"
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
