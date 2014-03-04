6596ab35e8a07d2f2fbabf281fae7838b7059a14
robinsonj <robinsonjosephlewis@gmail.com>
2014-03-03 16:05:15 -0800

    Expanded tests to cover devise.
    Two tests don't currently pass because error messages for user/signin aren't being shown. Not sure why, the application is still using the default devise forms so they messages should be there.
    Also including some devise test helpers necessary for fully testing the controllers.

---
+++ spec/requests/devise_users_requests_spec.rb
@@ -0,0 +1,73 @@
+require 'spec_helper'
+
+describe "DeviseUsers" do
+
+  before do
+    @user1  = User.create(name: 'test1', email: 'test1@test.com',  password: 'testtest')
+    @user2  = User.create(name: 'test2', email: 'test2@test.com',  password: 'testtest')
+  end
+
+  describe "devise creates a new user" do
+    it "has welcome message after devise/registrations#new with valid credentials" do
+      visit new_user_registration_path
+
+      fill_in "Email",                 :with => "test@test.com"
+      fill_in "Password",              :with => "testtest"
+      fill_in "Password confirmation", :with => "testtest"
+
+      click_button "Sign up"
+
+      page.should have_content("Welcome! You have signed up successfully.")
+    end
+
+    it "has error message after devise/registrations#new with invalid credentials" do
+      visit new_user_registration_path
+
+      fill_in "Email",                 :with => "test1@test.com"
+      fill_in "Password",              :with => "testtest"
+      fill_in "Password confirmation", :with => "testtest"
+
+      click_button "Sign up"
+
+      page.should have_content("errors prohibited this user from being saved")
+    end
+  end
+
+  describe "devise authenticates valid user" do
+    it "has sign in message after devise/sessions#new with valid credentials" do
+
+      visit new_user_session_path
+
+      fill_in "Email",    :with => "test2@test.com"
+      fill_in "Password", :with => "testtest"
+
+      click_button "Sign in"
+
+      page.should have_content("Signed in successfully.")
+    end
+
+    it "has error message after devise/sessions#new with invalid email" do
+
+      visit new_user_session_path
+
+      fill_in "Email",    :with => "test10@test.com"
+      fill_in "Password", :with => "testtest"
+
+      click_button "Sign in"
+
+      page.should have_content("error_message")
+    end
+
+    it "has error message after devise/sessions#new with invalid password" do
+
+      visit new_user_session_path
+
+      fill_in "Email",    :with => "test2@test.com"
+      fill_in "Password", :with => "test2test"
+
+      click_button "Sign in"
+
+      page.should have_content("error_message")
+    end
+  end
+end
