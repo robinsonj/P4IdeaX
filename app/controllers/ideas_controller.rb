82a45b20f6cf6ce1add9db97de6a2c447c22ae0e
robinsonj <robinsonjosephlewis@gmail.com>
2014-03-07 14:29:42 -0800

    Basic added actions for the ideas controller.
    Controller does nothing special when creating/editing ideas.

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,2 +1,34 @@
 class IdeasController < ApplicationController
+
+  # GET /ideas/new
+  def new
+    @idea = Idea.new
+  end
+
+  # POST /ideas
+  def create
+    @idea = Idea.new(idea_params)
+  end
+
+  # GET /ideas/1
+  def show
+  end
+
+  def index
+  end
+
+  # GET /users/1/edit
+  def edit
+    render :partial => 'edit'
+  end
+
+  def update
+
+  end
+
+  private
+
+    def idea_params
+      params.require(:idea).permit(:title, :description)
+    end
 end
