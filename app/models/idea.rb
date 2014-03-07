6064ff751007cdf56dc3582eeb763d6f7763a1df
robinsonj <robinsonjosephlewis@gmail.com>
2014-03-07 11:14:10 -0800

    Base idea attributes added.

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,2 +1,7 @@
 class Idea < ActiveRecord::Base
+
+  belongs_to :owner, :class_name => 'User'
+
+  validates_presence_of :title, :description
+
 end
--- db/migrate/20140307180717_create_ideas.rb
+++ db/migrate/20140307180717_create_ideas.rb
@@ -1,8 +1,16 @@
 class CreateIdeas < ActiveRecord::Migration
-  def change
-    create_table :ideas do |t|
+  def self.up
+    # Create the base ideas table, overriding one if it already exists.
+    create_table :ideas, :force => do |t|
+      t.text    "title"
+      t.text    "description"
+      t.integer "owner_id"
 
       t.timestamps
     end
+  end
+
+  def self.down
+    drop_table :ideas
   end
 end
