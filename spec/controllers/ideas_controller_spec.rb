793d12a5d42c918bda4117bea3d25571f2a0a348
robinsonj <robinsonjosephlewis@gmail.com>
2014-03-07 14:30:23 -0800

    Idea controller tests.
    Only test the basic GET requests right now. Others will come later when they have been added.

--- spec/controllers/ideas_controller_spec.rb
+++ spec/controllers/ideas_controller_spec.rb
@@ -1,5 +1,37 @@
 require 'spec_helper'
 
 describe IdeasController do
+  before do
+    @idea = create(:idea)
+  end
 
+  describe "GET 'index'" do
+    it "returns http success" do
+      get 'index'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'new'" do
+    it "returns http success" do
+      get 'new'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'edit'" do
+    it "returns http success" do
+      get 'edit', { id: 1 }
+      response.should be_success
+    end
+  end
+
+  describe "GET 'show'" do
+    it "returns http success" do
+      get 'show', { id: 1 }
+      response.should be_success
+    end
+  end
+
+  pending "has no POST tests yet"
 end
