676380262cd2855a9d6b2e2f2e59186888ceed42
robinsonj <robinsonjosephlewis@gmail.com>
2014-03-12 09:39:03 -0700

    Updated ordering of comments in idea model.
    Old ordering syntax is deprecated.

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,7 +2,7 @@ class Idea < ActiveRecord::Base
 
   belongs_to :owner, :class_name => 'User'
 
-  has_many :comments, :order => 'comments.created_at', :dependent => :destroy
+  has_many :comments, -> { order(created_at: :asc) }, :dependent => :destroy
 
   validates :title, presence: true, uniqueness: {case_sensitive: false}
   validates :description, presence: true
