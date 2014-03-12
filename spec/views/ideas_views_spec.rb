3fd6da651107cec4bb05b257a1d4139c606ef7b3
robinsonj <robinsonjosephlewis@gmail.com>
2014-03-12 16:16:55 -0700

    Basic tests for idea views rendering.
    Both the template 'new' and partial 'new' are tested to ensure the correct file is rendered.

---
+++ spec/views/ideas_views_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe "IdeaViews" do
+  describe "ideas/index.html.haml" do
+    it "renders ideas/index" do
+      render :template => "ideas/index"
+      expect(rendered).to render_template("ideas/index")
+    end
+  end
+
+  describe "ideas/new.html.haml" do
+    it "renders ideas/new" do
+      render :template => "ideas/new"
+      expect(rendered).to render_template("ideas/new")
+    end
+  end
+
+  describe "ideas/show.html.haml" do
+    it "renders ideas/show" do
+      render :template => "ideas/show"
+      expect(rendered).to render_template("ideas/show")
+      expect(rendered).not_to include "Remember, please keep it constructive!"
+    end
+  end
+
+  describe "ideas/_new.html.haml" do
+    it "renders ideas/_new" do
+      render :partial => "ideas/new"
+      expect(rendered).to render_template(:partial => "_new")
+      expect(rendered).to include "Remember, please keep it constructive!"
+    end
+  end
+end
