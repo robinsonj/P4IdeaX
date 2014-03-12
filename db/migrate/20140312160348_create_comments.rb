7536091948b93b51f069f37d22fc3b4eb1a77bd6
robinsonj <robinsonjosephlewis@gmail.com>
2014-03-12 09:12:53 -0700

    Created comments migration and updated schema.
    Comments only have very base attributes at the moment. Others can be added later but all that is needed now is an author, an associated idea, and the content text.

---
+++ db/migrate/20140312160348_create_comments.rb
@@ -0,0 +1,11 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments, :force => true do |t|
+      t.integer   "idea_id"
+      t.integer   "author_id"
+      t.text      "text"
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140307180717) do
+ActiveRecord::Schema.define(version: 20140312160348) do
+
+  create_table "comments", force: true do |t|
+    t.integer  "idea_id"
+    t.integer  "author_id"
+    t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "ideas", force: true do |t|
     t.text     "title"
@@ -39,5 +47,16 @@ ActiveRecord::Schema.define(version: 20140307180717) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "votes", force: true do |t|
+    t.datetime "updated_at"
+    t.datetime "created_at"
+    t.integer  "user_id"
+    t.integer  "idea_id"
+    t.decimal  "rating",     default: 0.0
+  end
+
+  add_index "votes", ["idea_id"], name: "index_votes_on_idea_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
