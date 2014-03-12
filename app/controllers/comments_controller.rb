d53d148ad237c3aa68cb1a7e79a8abbfbf44cf5b
robinsonj <robinsonjosephlewis@gmail.com>
2014-03-12 11:07:28 -0700

    Basic comments controller tests for GET actions.
    Only testing to see if actions are succeeding (response 200) and appropriate templates are being rendered.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,4 +2,24 @@ class CommentsController < ApplicationController
 
   before_filter :authenticate_user!, :except => [:index, :show]
 
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Commane.new(params)
+  end
+
+  def index
+    @comments = Comment.all
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
 end
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -2,4 +2,39 @@ require 'spec_helper'
 
 describe CommentsController do
 
+  before do
+    @user    = create(:user)
+    @idea    = create(:idea)
+    @comment = create(:comment)
+  end
+
+  describe "GET 'index'" do
+    it "returns http success" do
+      get 'index', { :idea_id => @idea.id }
+      response.should be_success
+    end
+  end
+
+  describe "GET 'new'" do
+    it "returns http success" do
+      get 'new', :idea_id => @idea.id
+      response.should be_success
+    end
+  end
+
+  describe "GET 'edit'" do
+    it "returns http success" do
+      get 'edit', { :idea_id => @idea.id, :id => @comment.id }
+      response.should be_success
+    end
+  end
+
+  describe "GET 'show'" do
+    it "returns http success" do
+      get 'show', { :idea_id => @idea.id, :id => @comment.id }
+      response.should be_success
+    end
+  end
+
+  pending "needs expanded GET, POST, PATCH, PUT, DESTROY tests"
 end
