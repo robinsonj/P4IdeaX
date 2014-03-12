8c47d316abe1a40289f5ee49397a93a073b74a38
robinsonj <robinsonjosephlewis@gmail.com>
2014-03-12 15:23:49 -0700

    Rspec tests for ideas helper method.
    Expected results will eventually change as right now the helper method tested returns all ideas. Eventually it should search ideas by specific params or return a limited number in a certain order (ex: most popular, recently added/new, trending, etc.).

--- spec/helpers/ideas_helper_spec.rb
+++ spec/helpers/ideas_helper_spec.rb
@@ -1,15 +1,31 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the IdeasHelper. For example:
-#
-# describe IdeasHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 describe IdeasHelper do
-  pending "No idea helper functions to test yet."
+  describe "idea_objects_list" do
+    it "fetches all ideas" do
+      idea1 = create(:idea, id: 1)
+      idea2 = create(:idea, id: 2)
+      idea3 = create(:idea, id: 3)
+      idea4 = build(:idea, id: 4)
+      idea5 = create(:idea, id: 5)
+
+      helper.idea_objects_list.should include idea1
+      helper.idea_objects_list.should include idea2
+      helper.idea_objects_list.should include idea3
+      helper.idea_objects_list.should_not include idea4
+      helper.idea_objects_list.should include idea5
+    end
+
+    it "should return an array (empty)" do
+      expect(helper.idea_objects_list).to eq([])
+    end
+
+    it "should return an array (not empty)" do
+      idea1 = create(:idea)
+      idea2 = create(:idea)
+
+      expect(helper.idea_objects_list).to match_array([idea1, idea2])
+      expect(helper.idea_objects_list).to match_array([idea2, idea1])
+    end
+  end
 end
