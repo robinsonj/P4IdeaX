af3bad0b1a451eba861676735cdf55876db8a9e3
robinsonj <robinsonjosephlewis@gmail.com>
2014-03-12 10:22:47 -0700

    Fixed 'before_filter' typo in comments controller.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
 
-  before_fiter :authenticate_user!, :except => [:index, :show]
+  before_filter :authenticate_user!, :except => [:index, :show]
 
 end
