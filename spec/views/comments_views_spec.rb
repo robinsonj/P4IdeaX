819174c28215c2d755940a76cb8bc810c17df3fd
robinsonj <robinsonjosephlewis@gmail.com>
2014-03-14 14:31:37 -0700

    Updated comment views tests.
    Fixed expectation messages for each test. Each still stated that it was
    trying to render the 'create' view, though this is incorrect and there
    is no longer a 'create' view. Also added expectations for what will be
    rendered on the page for the comment 'show' action to ensure that the
    comment contents are being correctly displated.

--- spec/views/comments_views_spec.rb
+++ spec/views/comments_views_spec.rb
@@ -3,32 +3,35 @@ require 'spec_helper'
 describe "CommentsViews" do
 
   describe "comments/edit.html.haml" do
-    it "renders create template" do
+    it "renders edit template" do
       render :template => "comments/edit"
       expect(view).to render_template("comments/edit")
     end
   end
 
   describe "comments/index.html.haml" do
-    it "renders create template" do
+    it "renders index template" do
       render :template => "comments/index"
       expect(view).to render_template("comments/index")
     end
   end
 
   describe "comments/new.html.haml" do
-    it "renders create template" do
+    it "renders new template" do
       render :template => "comments/new"
       expect(view).to render_template("comments/new")
     end
   end
 
   describe "comments/show.html.haml" do
-    it "renders create template" do
+    it "renders show template" do
       author   = create(:user)
       @comment = create(:comment, author_id: author.id)
       render :template => "comments/show"
+
       expect(view).to render_template("comments/show")
+      expect(rendered).to have_content(@comment.text)
+      expect(rendered).to have_content(@comment.author.name)
     end
   end
 end
