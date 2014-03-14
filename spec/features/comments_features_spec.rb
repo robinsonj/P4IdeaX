159cf709a9f895389208de9ff6c6739316c07855
robinsonj <robinsonjosephlewis@gmail.com>
2014-03-14 15:28:03 -0700

    Base feature tests for comments.
    Only test presence of content for now. Create some base instances of
    different models for testing.

--- spec/features/comments_features_spec.rb
+++ spec/features/comments_features_spec.rb
@@ -1,16 +1,28 @@
 require 'spec_helper'
+require 'faker'
 
 describe "CommentFeatures" do
   before do
-    @idea1 = create(:idea)
-    @idea2 = create(:idea)
+    @user1 = create(:user)
+    @user2 = create(:user)
+    @idea1 = create(:idea, owner_id: @user1.id)
+    @idea2 = create(:idea, owner_id: @user2.id)
+    @comment1_1 = create(:comment, idea_id: @idea1.id, author_id: @user1.id)
+    @comment1_2 = create(:comment, idea_id: @idea1.id, author_id: @user1.id)
   end
 
-  describe "write comments" do
-    it "should write and save new comments" do
+  describe "existing comments" do
+    it "shows existing comments" do
       visit idea_path(id: @idea1.id)
 
+      expect(page).to have_content(@comment1_1.text)
+      expect(page).to have_content(@comment1_2.text)
+    end
+  end
 
+  describe "new comments" do
+    it "should write and save new comments" do
+      visit new_idea_comment_path(idea_id: @idea1.id)
     end
   end
 end
