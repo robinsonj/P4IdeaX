bfe1d868e80277a035112743059a0ec5ca459298
robinsonj <robinsonjosephlewis@gmail.com>
2014-03-19 16:14:06 -0700

    Methods for finding/creating Tags
    There should be no duplicate tags stored in the database. Tags are
    retrieved as strings after being stripped of any extra characters. If
    the resulting string already exists in the Tags table, add that result.
    If it does not, create a new one and add that to the tags array
    instead.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,26 @@ class Tag < ActiveRecord::Base
   has_and_belongs_to_many :ideas
 
   validates :name, presence: true, uniqueness: {case_sensitive: false}
+
+  def from_string(tag_names)
+    tags = []
+
+    # Split the input string at commas, then strip any extra whitespace
+    # on each end of the substrings.
+    tag_names.split(/,/).strip.downcase.gsub(/ +/, '')each do |tag_name|
+      tags.push(find_or_create_by_name(tag_name)) if !tag_name.blank?
+    end
+
+    tags
+  end
+
+  private
+
+    def find_or_create_by_name(tag_name)
+      # Strip out any HTML tags that may have been slipped into the input.
+      tag_name = strip_tags(tag_name)
+
+      # Find the tag if it already exists, otherwise create a new one.
+      find_by_name(tag_name) || create!(:name => tag_name)
+    end
 end
