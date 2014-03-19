773cc3d0e73722f76b6ff4d272327b9d2c57ce3a
robinsonj <robinsonjosephlewis@gmail.com>
2014-03-19 10:41:10 -0700

    Specify idea_tags join table in Idea model
    The join table between ideas and tags defaults to 'ideas_tags.'
    Instead, 'idea_tags' is being used but the model didn't know to look
    for it. The 'join_table' option needs to be specified in the
    has_and_belongs_to_many association.

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -3,7 +3,9 @@ class Idea < ActiveRecord::Base
   belongs_to :owner, :class_name => 'User'
 
   has_many :comments, -> { order(created_at: :asc) }, :dependent => :destroy
-  has_and_belongs_to_many :tags, -> { order('name') }
+  has_and_belongs_to_many :tags,
+    -> { order('name') },
+    join_table: "idea_tags"
 
   validates :title, presence: true, uniqueness: {case_sensitive: false}
   validates :description, presence: true
