bddfd853bb4b936844c619110986529e8a4ea9cf
robinsonj <robinsonjosephlewis@gmail.com>
2014-03-21 11:12:41 -0700

    Add Devise confirmable module to Users
    Add the confirmable module to Users as part of Devise. Using the module
    will require users to confirm their accounts before being activated.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140317174950) do
+ActiveRecord::Schema.define(version: 20140321180750) do
 
   create_table "comments", force: true do |t|
     t.integer  "idea_id"
@@ -55,6 +55,9 @@ ActiveRecord::Schema.define(version: 20140317174950) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
