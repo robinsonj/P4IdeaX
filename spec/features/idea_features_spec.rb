736909d825cb83f2a24e13bff0eeb6b036fde896
robinsonj <robinsonjosephlewis@gmail.com>
2014-03-21 15:06:34 -0700

    Test idea features with registered user
    Before requesting the new idea page, sign in with a registered and
    confirmed test user.

--- spec/features/idea_features_spec.rb
+++ spec/features/idea_features_spec.rb
@@ -2,8 +2,20 @@ require 'spec_helper'
 require 'faker'
 
 describe "IdeaFeatures" do
-  describe "Creating new ideas" do
+  describe "Create new ideas" do
+    before do
+      @user = create(:user)
+      @user.confirm!
+    end
+
     it "should create a new idea" do
+      visit new_user_session_path
+
+      fill_in "Email",    :with => "#{@user.email}"
+      fill_in "Password", :with => "#{@user.password}"
+
+      click_button "Sign in"
+
       visit new_idea_path
 
       fill_in "new-idea-title",         :with => Faker::Lorem.sentence
