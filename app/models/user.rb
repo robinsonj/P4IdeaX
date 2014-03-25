c798724252b5da85d67b2a6792cc704253b02d9c
robinsonj <robinsonjosephlewis@gmail.com>
2014-03-25 14:44:29 -0700

    Add name and password validations to User model
    Users are requires to have a unique name and an encrypted password.
    Rails should not attempt to create a record if these fields are not
    filled out in the devise registration form.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
 
+  validates :name, presence: true, uniqueness: {case_sensitive: false}
   validates :email, presence: true, uniqueness: {case_sensitive: false}
+  validates :encrypted_password, presence: true
 
   has_many :comments, :foreign_key => 'author_id'
 end
