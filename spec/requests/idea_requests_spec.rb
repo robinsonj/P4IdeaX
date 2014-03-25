d9b8c233f884118e566b992fedf6f61529a06d9c
robinsonj <robinsonjosephlewis@gmail.com>
2014-03-25 13:01:00 -0700

    Add 'should' to idea request expectation message
    Make expectations of tests a little more clear.

--- spec/requests/idea_requests_spec.rb
+++ spec/requests/idea_requests_spec.rb
@@ -7,7 +7,7 @@ describe "IdeaRequests" do
   end
 
   describe "GET /ideas" do
-    it "return http success" do
+    it "should return http success" do
       get ideas_path
       expect(response).to be_success
     end
@@ -20,21 +20,21 @@ describe "IdeaRequests" do
       post_via_redirect user_session_path, 'user[email]' => @user.email, 'user[password]' => @user.password
     end
 
-    it "return http success" do
+    it "should return http success" do
       get new_idea_path
       expect(response).to be_success
     end
   end
 
   describe "GET /ideas/:id/edit" do
-    it "return http success" do
+    it "should return http success" do
       get edit_idea_path(:id => @idea1.id)
       expect(response).to be_success
     end
   end
 
   describe "GET /ideas/:id" do
-    it "return http success" do
+    it "should return http success" do
       get idea_path(:id => @idea2.id)
       expect(response).to be_success
     end
