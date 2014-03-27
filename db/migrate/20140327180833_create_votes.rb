2aa873a58c5017d28edd5f8e129a0aef5aa27af1
robinsonj <robinsonjosephlewis@gmail.com>
2014-03-27 11:12:50 -0700

    Add Votes migration file
    Add a 'votes' table and a rating table to the database. Votes have two
    owners: an idea and a user each referenced by their respective ID's.

---
+++ db/migrate/20140327180833_create_votes.rb
@@ -0,0 +1,18 @@
+class CreateVotes < ActiveRecord::Migration
+  def self.up
+    create_table :votes, :id => false, :force => true do |t|
+      t.references  :user
+      t.references  :idea
+      t.integer     :value, :null => false
+      t.timestamps
+    end
+
+    add_column :ideas, :rating, :integer, :default => 0
+  end
+
+  def self.down
+    drop_table :votes
+    remove_column :ideas, :rating
+
+  end
+end
