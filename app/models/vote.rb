c5dbd8f080b44338b775ec587442f73adb083eb9
robinsonj <robinsonjosephlewis@gmail.com>
2014-03-31 12:44:33 -0700

    Fix Vote model validations
    Vote records should have a 'value' and a unique 'user_id, idea_id' key.
    Votes use the 'composite_primary_keys' gem to allow the use of two
    fields as a composite key in the database in place of the default 'id'
    fields Rails uses.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,8 +5,10 @@ class Vote < ActiveRecord::Base
   belongs_to :idea
   belongs_to :user
 
-  validates :user, uniqueness: {
-    scope: :idea,
+  validates :value, presence: true
+  validates :idea_id,  presence: true
+  validates :user_id,  presence: true, uniqueness: {
+    scope: :idea_id,
     message: "A user cannot vote on the same idea twice."
   }
 end
