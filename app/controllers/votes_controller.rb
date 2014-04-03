f92e6a5ddb312bc99b8cfd849960b8e0cf3e6a9f
robinsonj <robinsonjosephlewis@gmail.com>
2014-04-02 18:38:46 -0700

    Fill out Votes controller actions
    Fill in the 'create,' 'update,' and 'destroy' actions to modify vote
    resources belonging to a particular idea and user. Each action calls a
    method on the Idea to be modified in order to increment or decrement
    the Idea's rating.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,20 +1,56 @@
 class VotesController < ApplicationController
 
-  def create
+  before_filter :authenticate_user!
 
+  def create
+    @idea = Idea.find(params[:id])
+    @vote = Vote.new(vote_params)
+
+    if @vote.save!
+      @idea.add_vote(@vote)
+      flash[:notice] = 'Your vote was saved.'
+      render template: 'ideas/show', status: 201
+    else
+      flash[:notice] = 'Your vote could not be saved.'
+      render template: 'ideas/show'
+    end
   end
 
   def update
+    @idea = Idea.find(params[:id])
+    @vote = Vote.find(@idea.id, current_user.id)
+    @vote.value = params[:value]
 
+    if @vote.save!
+      @idea.subtract_vote(@vote)
+      @idea.add_vote(@vote)
+      flash[:notice] = 'Your vote was saved.'
+      render template: 'ideas/show'
+    else
+      flash[:notice] = 'Your vote could not be saved.'
+      render template: 'ideas/show'
+    end
   end
 
   def destroy
+    @idea = Idea.find(params[:id])
+    @vote = Vote.find(@idea.id, current_user.id)
+    @idea.subtract_vote(@vote)
 
+    if @vote.destroy!
+      flash[:notice] = 'Your vote was removed.'
+      render template: 'ideas/show'
+    else
+      flash[:notice] = 'Your vote could not be removed.'
+      render template: 'ideas/show'
+    end
   end
 
   private
 
     def vote_params
-      params.require(:idea).permit(:user_id, :idea_id, :value)
+      params.require(:id)
+      params.require(:value)
+      { :idea_id => params[:id], :user_id => current_user.id, :value => params[:value] }
     end
 end
