9ad7ddc0c7d2232e651a93f0d2dc1d742e4c266a
robinsonj <robinsonjosephlewis@gmail.com>
2014-04-07 14:55:21 -0700

    Create migration for adding Currents to schema
    Migration adds the Currents table and a column in ideas to reference a
    current the idea belongs to.

--- db/migrate/20140407202830_create_currents.rb
+++ db/migrate/20140407202830_create_currents.rb
@@ -1,8 +1,18 @@
 class CreateCurrents < ActiveRecord::Migration
-  def change
+  def self.up
     create_table :currents do |t|
+      t.string      "title"
+      t.text        "description"
+      t.integer     "owner_id",    :null => false
 
       t.timestamps
     end
+
+    add_column :ideas, :current_id, :integer, :null => true
+  end
+
+  def self.down
+    drop_table :currents
+    remove_column :ideas, :current_id
   end
 end
