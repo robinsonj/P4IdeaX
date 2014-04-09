574b6ffc5e840e0cb988eeb6038411bc12bcbbd0
robinsonj <robinsonjosephlewis@gmail.com>
2014-04-09 16:03:39 -0700

    Set valid and invalid attributes for Currents
    Use attribute hashes to pass factory-generated values to the Currents
    controller for testing. Invalid attributes only assigns the Current's
    title to be 'nil.'

--- spec/controllers/currents_controller_spec.rb
+++ spec/controllers/currents_controller_spec.rb
@@ -23,7 +23,10 @@ describe CurrentsController do
   # This should return the minimal set of attributes required to create a valid
   # Current. As you add validations to Current, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { } }
+  let(:valid_attributes) { FactoryGirl.attributes_for(:current) }
+
+  # Test some actions with invalid attributes.
+  let(:invalid_attributes) { FactoryGirl.attributes_for(:current, :title => nil) }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
