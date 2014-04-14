2ea4c47d8e047109001a93ed58bfc1f88af40a4d
robinsonj <robinsonjosephlewis@gmail.com>
2014-04-14 14:20:18 -0700

    Fill in some of the Twitter auth callback controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,17 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def twitter
+    puts auth_hash.inspect
 
+    if false
+      render home_index
+    else
+      redirect_to new_user_registration_path
+    end
   end
+
+  private
+
+    def auth_hash
+      request.env["omniauth.auth"]
+    end
 end
