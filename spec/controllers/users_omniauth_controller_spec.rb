03d4fc7b6580529b0e76eb7b3c2a72496ef92065
robinsonj <robinsonjosephlewis@gmail.com>
2014-04-14 17:25:37 -0700

    Add pending omniauth specs

---
+++ spec/controllers/users_omniauth_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Users::OmniauthCallbacksController do
+  describe "Twitter Omniauth Authentication" do
+    pending "Twitter specs"
+  end
+
+  describe "Facebook Omniauth Authentication" do
+    pending "Facebook specs"
+  end
+end
