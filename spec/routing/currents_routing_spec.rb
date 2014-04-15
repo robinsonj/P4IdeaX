c7a696b76ba08a020528b64d76fa3ed5338e45d9
robinsonj <robinsonjosephlewis@gmail.com>
2014-04-14 18:24:20 -0700

    Fix Currents routing specs
      * Fix expectation syntax of tests. Use 'expect(...).to' over
    '....should.'
      * Add a 'put' test to the spec

--- spec/routing/currents_routing_spec.rb
+++ spec/routing/currents_routing_spec.rb
@@ -4,31 +4,35 @@ describe CurrentsController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/currents").should route_to("currents#index")
+      expect(:get => "/currents").to route_to("currents#index")
     end
 
     it "routes to #new" do
-      get("/currents/new").should route_to("currents#new")
+      expect(:get => "/currents/new").to route_to("currents#new")
     end
 
     it "routes to #show" do
-      get("/currents/1").should route_to("currents#show", :id => "1")
+      expect(:get => "/currents/1").to route_to("currents#show", :id => "1")
     end
 
     it "routes to #edit" do
-      get("/currents/1/edit").should route_to("currents#edit", :id => "1")
+      expect(:get => "/currents/1/edit").to route_to("currents#edit", :id => "1")
     end
 
     it "routes to #create" do
-      post("/currents").should route_to("currents#create")
+      expect(:post => "/currents").to route_to("currents#create")
     end
 
     it "routes to #update" do
-      put("/currents/1").should route_to("currents#update", :id => "1")
+      expect(:patch => "/currents/1").to route_to("currents#update", :id => "1")
+    end
+
+    it "routes to #update" do
+      expect(:put => "/currents/1").to route_to("currents#update", :id => "1")
     end
 
     it "routes to #destroy" do
-      delete("/currents/1").should route_to("currents#destroy", :id => "1")
+      expect(:delete => "/currents/1").to route_to("currents#destroy", :id => "1")
     end
 
   end
