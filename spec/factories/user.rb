93b88bbbc777aef82fc8acfccfc49e94dad1a913
robinsonj <robinsonjosephlewis@gmail.com>
2014-04-18 16:49:13 -0700

    Fill out facebook user factory
    Fill in necessary attributes for a user authenticating through
    Facebook.

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,9 +2,9 @@ require 'faker'
 
 FactoryGirl.define do
   factory :user do
-    name { Faker::Internet.user_name }
-    email { Faker::Internet.email }
-    password { Faker::Lorem.characters(20) }
+    name      { Faker::Internet.user_name }
+    email     { Faker::Internet.email }
+    password  { Faker::Lorem.characters(20) }
   end
 
   factory :twitter_user, class: User do
@@ -14,6 +14,10 @@ FactoryGirl.define do
     uid       { rand(100000000).to_s }
   end
 
-  factory :facebook_user do
+  factory :facebook_user, class: User do
+    name      { Faker::Internet.user_name }
+    password  { Devise.friendly_token }
+    provider  { 'facebook' }
+    uid       { rand(100000000).to_s }
   end
 end
