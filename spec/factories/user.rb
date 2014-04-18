429162e409c0f28db05be33ee026d8b179733b5f
robinsonj <robinsonjosephlewis@gmail.com>
2014-04-18 13:54:20 -0700

    Update User model spec and factories
    User model specs were never updated to user the user factory. Adjust
    the specs to use it and more exhaustively check the validitiy of user
    records.
    
    Add tests for users registered though third parties with omniauth and
    the method for finding or creating them.

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -6,4 +6,14 @@ FactoryGirl.define do
     email { Faker::Internet.email }
     password { Faker::Lorem.characters(20) }
   end
+
+  factory :twitter_user, class: User do
+    name      { Faker::Internet.user_name }
+    password  { Devise.friendly_token }
+    provider  { 'twitter' }
+    uid       { rand(100000000).to_s }
+  end
+
+  factory :facebook_user do
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,26 +2,81 @@ require 'spec_helper'
 
 describe User do
 
-  before do
-    @user = User.new(name: 'test', email: 'test@test.com', password: 'testtest')
-    @user2 = User.new(name: 'test2', email: 'test2@test.com', password: '')
-  end
+  describe "New standard Users" do
 
-  subject { @user }
-
-  describe "creation with duplicate email address" do
-    before do
-      duplicate_user = @user.dup
-      duplicate_user.email = @user.email.upcase
-      duplicate_user.save
+    it "creates new users with valid params" do
+      user = build(:user)
+      expect(user).to be_valid
     end
 
-    it { should_not be_valid }
+    it "does not create users without a password" do
+      user1 = build(:user, :password => '')
+      user2 = build(:user, :password => nil)
+      expect(user1).not_to be_valid
+      expect(user2).not_to be_valid
+    end
+
+    it "does not create duplicate users (email)" do
+      user1 = create(:user)
+      user2 = build(:user, :email => user1.email)
+      expect(user1).to      be_valid
+      expect(user2).not_to  be_valid
+    end
+
+    it "does not create duplicate users (name)" do
+      user1 = create(:user)
+      user2 = build(:user, :name => user1.name)
+      expect(user1).to      be_valid
+      expect(user2).not_to  be_valid
+    end
   end
 
-  subject { @user2 }
+  describe "New Omniauth-Twitter Users" do
+    it "creates new users with valid params" do
+      user = build(:twitter_user)
+      expect(user).to be_valid
+    end
 
-  describe "creation with blank password" do
-    it { should_not be_valid }
+    it "does not create users without a provider" do
+      user1 = build(:twitter_user, :provider => '')
+      user2 = build(:twitter_user, :provider => nil)
+      expect(user1).not_to be_valid
+      expect(user2).not_to be_valid
+    end
+
+    it "does not create duplicate users (provider + uid)" do
+      user1 = create(:twitter_user)
+      user2 = build(:twitter_user, :uid => user1.uid)
+      expect(user1).to      be_valid
+      expect(user2).not_to  be_valid
+    end
+  end
+
+  describe "New Omniauth-Facebook Users" do
+    pending "Omniauth-Facebook implementation"
+  end
+
+  describe "Oauth records" do
+
+    it "finds existing omniauth users" do
+      user = create(:twitter_user)
+      info = { :nickname => user.name }
+
+      result = User.find_by_oauth(user.provider, user.uid, info)
+
+      expect(result).to eq(user)
+      expect(result).to be_valid
+    end
+
+    it "creates new omniauth users" do
+      user = build(:twitter_user)
+      provider  = user.provider
+      uid       = user.uid
+      info      = { :nickname => user.name }
+
+      result = User.find_by_oauth(provider, uid, info)
+
+      expect(result).to be_valid
+    end
   end
 end
