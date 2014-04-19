8ee0a1a938695b9810abc45b3076e1934831fcb5
robinsonj <robinsonjosephlewis@gmail.com>
2014-04-18 17:10:21 -0700

    Expand User model Oauth specs
    Test Facebook authorized user records.
    
    Currently, the user table has a failure in indexing over the user email
    column. Normally, all users would have an email address but
    authenticating through Twitter or Facebook doesn't provide one (unless
    specifically requested for in the case of Facebook). Without an email
    address, the email index fails. The index can be removed, but it would
    significantly slow down the access of records that do authenticate
    normally through devise.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -44,6 +44,13 @@ describe User do
       expect(user2).not_to be_valid
     end
 
+    it "does not create users without a uid" do
+      user1 = build(:twitter_user, :uid => '')
+      user2 = build(:twitter_user, :uid => nil)
+      expect(user1).not_to be_valid
+      expect(user2).not_to be_valid
+    end
+
     it "does not create duplicate users (provider + uid)" do
       user1 = create(:twitter_user)
       user2 = build(:twitter_user, :uid => user1.uid)
@@ -53,30 +60,86 @@ describe User do
   end
 
   describe "New Omniauth-Facebook Users" do
-    pending "Omniauth-Facebook implementation"
+    it "creates new users with valid params" do
+      user = build(:facebook_user)
+      expect(user).to be_valid
+    end
+
+    it "does not create users without a provider" do
+      user1 = build(:facebook_user, :provider => '')
+      user2 = build(:facebook_user, :provider => nil)
+      expect(user1).not_to be_valid
+      expect(user2).not_to be_valid
+    end
+
+    it "does not create users without a uid" do
+      user1 = build(:facebook_user, :uid => '')
+      user2 = build(:facebook_user, :uid => nil)
+      expect(user1).not_to be_valid
+      expect(user2).not_to be_valid
+    end
+
+    it "does not create duplicate users (provider + uid)" do
+      user1 = create(:facebook_user)
+      user2 = build(:facebook_user, :uid => user1.uid)
+      expect(user1).to      be_valid
+      expect(user2).not_to  be_valid
+    end
   end
 
   describe "Oauth records" do
+    describe "Twitter users" do
+      it "finds existing omniauth-twitter users" do
+        user = create(:twitter_user)
+        info = { :nickname => user.name }
 
-    it "finds existing omniauth users" do
-      user = create(:twitter_user)
-      info = { :nickname => user.name }
+        result = User.find_by_oauth(user.provider, user.uid, info)
 
-      result = User.find_by_oauth(user.provider, user.uid, info)
+        expect(result).to eq(user)
+        expect(result).to be_valid
+      end
 
-      expect(result).to eq(user)
-      expect(result).to be_valid
+      it "creates new omniauth-twitter users" do
+        user = build(:twitter_user)
+        provider  = user.provider
+        uid       = user.uid
+        info      = { :nickname => user.name }
+
+        result = User.find_by_oauth(provider, uid, info)
+
+        expect(result).to be_valid
+      end
     end
 
-    it "creates new omniauth users" do
-      user = build(:twitter_user)
-      provider  = user.provider
-      uid       = user.uid
-      info      = { :nickname => user.name }
+    describe "Facebook users" do
+      it "finds existing omniauth-facebook users" do
+        user = create(:facebook_user)
+        info = { :nickname => user.name }
 
-      result = User.find_by_oauth(provider, uid, info)
+        result = User.find_by_oauth(user.provider, user.uid, info)
 
-      expect(result).to be_valid
+        expect(result).to eq(user)
+        expect(result).to be_valid
+      end
+
+      it "creates new omniauth-facebook users" do
+        user = build(:facebook_user)
+        provider  = user.provider
+        uid       = user.uid
+        info      = { :nickname => user.name }
+
+        result = User.find_by_oauth(provider, uid, info)
+
+        expect(result).to be_valid
+      end
+    end
+
+    it "allows users with identical uids but different providers" do
+      twitter_user  = create(:twitter_user)
+      facebook_user = create(:facebook_user, :uid => twitter_user.uid)
+
+      expect(twitter_user).to   be_valid
+      expect(facebook_user).to  be_valid
     end
   end
 end
