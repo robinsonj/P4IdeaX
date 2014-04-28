b8c29481f2e61799e040366d4169c2e2f18bb8fd
robinsonj <robinsonjosephlewis@gmail.com>
2014-04-28 11:28:14 -0700

    User factories should accept the ToU
    When creating a user record, pass in the acceptance of the ToU so it
    passes the model validation. Without the factory parameter, valid user
    records can't be created so all tests involving them will fail.

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,22 +2,25 @@ require 'faker'
 
 FactoryGirl.define do
   factory :user do
-    name      { Faker::Internet.user_name }
-    email     { Faker::Internet.email }
-    password  { Faker::Lorem.characters(20) }
+    name          { Faker::Internet.user_name }
+    email         { Faker::Internet.email }
+    password      { Faker::Lorem.characters(20) }
+    terms_of_use  { "1" }
   end
 
   factory :twitter_user, class: User do
-    name      { Faker::Internet.user_name }
-    password  { Devise.friendly_token }
-    provider  { 'twitter' }
-    uid       { rand(100000000).to_s }
+    name          { Faker::Internet.user_name }
+    password      { Devise.friendly_token }
+    provider      { 'twitter' }
+    uid           { rand(100000000).to_s }
+    terms_of_use  { "1" }
   end
 
   factory :facebook_user, class: User do
-    name      { Faker::Internet.user_name }
-    password  { Devise.friendly_token }
-    provider  { 'facebook' }
-    uid       { rand(100000000).to_s }
+    name          { Faker::Internet.user_name }
+    password      { Devise.friendly_token }
+    provider      { 'facebook' }
+    uid           { rand(100000000).to_s }
+    terms_of_use  { "1" }
   end
 end
