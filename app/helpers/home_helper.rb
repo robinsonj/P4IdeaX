d189b089f4986c214c415614b2a5b7c99e049a78
robinsonj <robinsonjosephlewis@gmail.com>
2014-04-28 17:18:39 -0700

    Show new and popular ideas on the front page
    Create:
      - Home helper methods to retrieve the most recent/most popular open
        ideas.
      - Tiny '_idea' partial in the home views to show small idea excerpts.
    
    Fix:
      - Render the partial with appropriate arguments (how many ideas to
        render, what to render, which partial to render).

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,10 @@
 module HomeHelper
+
+  def recent_ideas(max = 5)
+    @ideas = Idea.where(status: 'new').order(created_at: :desc).first(max)
+  end
+
+  def popular_ideas(max = 5)
+    @ideas = Idea.where(status: 'new' || 'discussion').order(rating: :desc).first(max)
+  end
 end
