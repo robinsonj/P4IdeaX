6ef3940fa57728c4d6872fe061327bb88fb4eadb
robinsonj <robinsonjosephlewis@gmail.com>
2014-05-05 11:42:21 -0700

    Update Tag controller specs
    Tags should be shown to users both registered and unregistered.
    Add a test for the 'show' action ensuring the targeted Tag is the one
    rendered in the view.

--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -1,10 +1,54 @@
 require 'spec_helper'
 
 describe TagsController do
+  let(:valid_session)   { { "warden.user.user.key" => session["warden.user.user.key"] } }
+  let(:invalid_session) { { } }
+
+  before(:each) do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+
+    @user = create(:user)
+    @user.confirm!
+    sign_in @user
+  end
+
   describe "GET 'index'" do
-    it "should render 'index' template" do
-      get 'index'
-      expect(response).to render_template('index')
+    describe "valid user session" do
+      it "assigns all tags as @tags" do
+        tag = create(:tag)
+        get :index, {}, valid_session
+
+        expect(assigns(:tags)).to eq([tag])
+      end
+    end
+
+    describe "invalid user session" do
+      it "assigns all tags as @tags" do
+        tag = create(:tag)
+        get :index, {}, invalid_session
+
+        expect(assigns(:tags)).to eq([tag])
+      end
+    end
+  end
+
+  describe "GET 'show'" do
+    describe "valid user session" do
+      it "assigns all tags as @tags" do
+        tag = create(:tag)
+        get :show, { :id => tag.to_param }, valid_session
+
+        expect(assigns(:tag)).to eq(tag)
+      end
+    end
+
+    describe "invalid user session" do
+      it "assigns all tags as @tags" do
+        tag = create(:tag)
+        get :show, { :id => tag.to_param }, invalid_session
+
+        expect(assigns(:tag)).to eq(tag)
+      end
     end
   end
 end
