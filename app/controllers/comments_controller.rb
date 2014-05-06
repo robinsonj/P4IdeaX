0c20bf8e4167c4aa4cc383ae424f2c192b73e18b
robinsonj <robinsonjosephlewis@gmail.com>
2014-05-05 17:20:46 -0700

    Update Comments controller
    Add actions for editing, updating, and deleting comment records. Fill
    in comments to clarify what routes are routing to the controller
    actions.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,14 @@
 class CommentsController < ApplicationController
 
-  before_filter :authenticate_user!, :except => [:index, :show]
+  before_action :set_comment, :only => [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, :except => [:index, :show]
 
+  # GET /ideas/:idea_id/comments/new
   def new
     @comment = Comment.new
   end
 
+  # POST /ideas/:idea_id/comments
   def create
     @idea = Idea.friendly.find(params[:idea_id])
     @comment = Comment.new(comment_params)
@@ -19,15 +22,42 @@ class CommentsController < ApplicationController
     end
   end
 
+  # GET /ideas/:idea_id/comments/:id
   def show
-    @comment = Comment.find(params[:id])
   end
 
+  # GET /ideas/:idea_id/comments/:id/edit
   def edit
-    @comment = Comment.find(params[:id])
+  end
+
+  # PATCH /ideas/:idea_id/comments/:id
+  # PUT   /ideas/:idea_id/comments/:id
+  def update
+
+    if @comment.update(comment_params)
+      redirect_to @comment.idea, notice: 'Your comment was successfully updated.'
+    else
+      redirect_to @comment.idea, notice: 'Your comment was not successfully updated.'
+    end
+  end
+
+  # DELETE /ideas/:idea_id/comments/:id
+  def destroy
+    @idea = @comment.idea
+
+    if @comment.destroy
+      redirect_to idea_path(@idea), notice: 'Your comment was successfully removed.'
+    else
+      redirect_to idea_path(@idea), notice: 'Your comment could not be removed.'
+    end
+
   end
 
   private
+
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
 
     def comment_params
       params.require(:comment).permit(:text)
