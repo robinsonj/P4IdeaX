cb1a7573456728b83aa201bf7ca54dfa7e066a6e
robinsonj <robinsonjosephlewis@gmail.com>
2014-05-07 13:01:02 -0700

    Run User admin DB migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,101 +11,102 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_140_428_230_527) do
+ActiveRecord::Schema.define(version: 20140507195522) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'comments', force: true do |t|
-    t.integer 'idea_id'
-    t.integer 'author_id'
-    t.text 'text'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.boolean 'hidden',     default: false, null: false
+  create_table "comments", force: true do |t|
+    t.integer  "idea_id"
+    t.integer  "author_id"
+    t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "hidden",     default: false, null: false
   end
 
-  create_table 'currents', force: true do |t|
-    t.string 'title'
-    t.text 'description'
-    t.integer 'owner_id',    null: false
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
+  create_table "currents", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "owner_id",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table 'friendly_id_slugs', force: true do |t|
-    t.string 'slug',                      null: false
-    t.integer 'sluggable_id',              null: false
-    t.string 'sluggable_type', limit: 50
-    t.string 'scope'
-    t.datetime 'created_at'
+  create_table "friendly_id_slugs", force: true do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
   end
 
-  add_index 'friendly_id_slugs', %w(slug sluggable_type scope), name: 'index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope', unique: true, using: :btree
-  add_index 'friendly_id_slugs', %w(slug sluggable_type), name: 'index_friendly_id_slugs_on_slug_and_sluggable_type', using: :btree
-  add_index 'friendly_id_slugs', ['sluggable_id'], name: 'index_friendly_id_slugs_on_sluggable_id', using: :btree
-  add_index 'friendly_id_slugs', ['sluggable_type'], name: 'index_friendly_id_slugs_on_sluggable_type', using: :btree
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
 
-  create_table 'idea_tags', id: false, force: true do |t|
-    t.integer 'tag_id'
-    t.integer 'idea_id'
+  create_table "idea_tags", id: false, force: true do |t|
+    t.integer "tag_id"
+    t.integer "idea_id"
   end
 
-  add_index 'idea_tags', ['idea_id'], name: 'index_idea_tags_on_idea_id', using: :btree
-  add_index 'idea_tags', ['tag_id'], name: 'index_idea_tags_on_tag_id', using: :btree
+  add_index "idea_tags", ["idea_id"], name: "index_idea_tags_on_idea_id", using: :btree
+  add_index "idea_tags", ["tag_id"], name: "index_idea_tags_on_tag_id", using: :btree
 
-  create_table 'ideas', force: true do |t|
-    t.text 'title'
-    t.text 'description'
-    t.integer 'owner_id'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.integer 'rating',      default: 0
-    t.integer 'current_id'
-    t.boolean 'closed',      default: false, null: false
-    t.string 'slug',                        null: false
-    t.string 'status',      default: 'new'
+  create_table "ideas", force: true do |t|
+    t.text     "title"
+    t.text     "description"
+    t.integer  "owner_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "rating",      default: 0
+    t.integer  "current_id"
+    t.boolean  "closed",      default: false, null: false
+    t.string   "slug",                        null: false
+    t.string   "status",      default: "new"
   end
 
-  create_table 'tags', force: true do |t|
-    t.string 'name'
+  create_table "tags", force: true do |t|
+    t.string "name"
   end
 
-  create_table 'users', force: true do |t|
-    t.string 'name'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.string 'email',                  default: ''
-    t.string 'encrypted_password',     default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.integer 'sign_in_count',          default: 0,  null: false
-    t.datetime 'current_sign_in_at'
-    t.datetime 'last_sign_in_at'
-    t.string 'current_sign_in_ip'
-    t.string 'last_sign_in_ip'
-    t.string 'confirmation_token'
-    t.datetime 'confirmed_at'
-    t.datetime 'confirmation_sent_at'
-    t.string 'unconfirmed_email'
-    t.string 'provider'
-    t.string 'uid'
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "email",                  default: ""
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,     null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.string   "provider"
+    t.string   "uid"
+    t.boolean  "admin",                  default: false
   end
 
-  add_index 'users', ['email'], name: 'index_users_on_email', using: :btree
-  add_index 'users', %w(provider uid), name: 'index_users_on_provider_and_uid', unique: true, using: :btree
-  add_index 'users', ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", using: :btree
+  add_index "users", ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  create_table 'votes', id: false, force: true do |t|
-    t.integer 'user_id'
-    t.integer 'idea_id'
-    t.integer 'value',      null: false
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
+  create_table "votes", id: false, force: true do |t|
+    t.integer  "user_id"
+    t.integer  "idea_id"
+    t.integer  "value",      null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index 'votes', ['idea_id'], name: 'index_votes_on_idea_id', using: :btree
-  add_index 'votes', ['user_id'], name: 'index_votes_on_user_id', using: :btree
+  add_index "votes", ["idea_id"], name: "index_votes_on_idea_id", using: :btree
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id", using: :btree
 
 end
