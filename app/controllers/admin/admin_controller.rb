688829ca62a49c1520cfa3d01d40d2df0baac18a
robinsonj <robinsonjosephlewis@gmail.com>
2014-05-09 10:23:07 -0700

    Fix admin auth ordering
    Change the ordering of current user authorization/authentication:
      * Check if the current user is an admin first. Check needs to be done
        with '.try(:admin?)' in case there is not current user.
      * If the current user is an admin, it will always pass the next auth
        check by devise (or should, unless something strange is happening).
    
    Only if a user is authenticated and an admin should it be allowed to
    see the admin interface.

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,13 +1,13 @@
 class Admin::AdminController < ApplicationController
   layout 'admin'
 
-  before_action :authenticate_user!
   before_action :require_admin
+  before_action :authenticate_user!
 
   private
 
   def require_admin
     # redirect_to root_path, notice: "#{t 'authorization.admin.unauthorized'}" unless current_user.admin?
-    redirect_to root_path unless current_user.admin?
+    redirect_to root_path unless current_user.try(:admin?)
   end
 end
