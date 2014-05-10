cc32f6598c1ede816ba77b8d211e12bcc395e3eb
robinsonj <robinsonjosephlewis@gmail.com>
2014-05-09 18:33:34 -0700

    Create admin Ideas controller
    Some actions still need to be filled out. Admins will have more control
    over the creation and editing of Idea records however.

---
+++ app/controllers/admin/ideas_controller.rb
@@ -0,0 +1,57 @@
+class Admin::IdeasController < Admin::AdminController
+
+  before_action :set_idea, only: [:edit, :update, :destroy]
+
+  # GET /admin/ideas/new
+  def new
+    @idea = Idea.new
+  end
+
+  # GET /admin/ideas
+  def index
+    @ideas = Idea.all
+  end
+
+  # POST /admin/ideas
+  def create
+    @idea = Idea.new(idea_params)
+
+    if idea_tag_params
+      @idea.tags += Tag.from_string(idea_tag_params)
+    end
+
+    if @idea.save
+
+    end
+
+  end
+
+  # GET /admin/ideas/:id/edit
+  def edit
+
+  end
+
+  # PATCH/PUT /admin/ideas/:id
+  def update
+
+  end
+
+  # DELETE /admin/ideas/:id
+  def destroy
+    @idea.destroy
+  end
+
+  private
+
+  def set_idea
+    @idea = Idea.find(params[:id])
+  end
+
+  def idea_params
+    params.require(:idea).permit(:owner, :title, :description, :current)
+  end
+
+  def idea_tag_params
+    params.require(:idea).permit(tag_names: [])[:tag_names]
+  end
+end
