9f5d97971e193e860f15f599ade9bf04b74c1476
robinsonj <robinsonjosephlewis@gmail.com>
2014-05-14 11:57:07 -0700

    Update Admin Idea request specs
      * Remove 'webrat' generation comment
      * Add pending message for further tests

--- spec/requests/admin/admin_ideas_spec.rb
+++ spec/requests/admin/admin_ideas_spec.rb
@@ -14,9 +14,10 @@ describe "Admin::Ideas" do
     end
 
     it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       get admin_ideas_path
       response.status.should be(200)
     end
   end
+
+  pending "expand Admin Idea request specs #{__FILE__}"
 end
