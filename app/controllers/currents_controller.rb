2456c2d473c06a81688edbb0f27fc3f86c6d0b3b
robinsonj <robinsonjosephlewis@gmail.com>
2014-05-14 13:50:49 -0700

    Remove CRUD actions from app Currents controller
    Current records should not be manageable from the regular application
    interface. Remove all but the 'index' and 'show' actions from its
    controller. Current records will be managed by admins through their
    own Currents controller.

--- app/controllers/currents_controller.rb
+++ app/controllers/currents_controller.rb
@@ -1,5 +1,5 @@
 class CurrentsController < ApplicationController
-  before_action :set_current, only: [:show, :edit, :update, :destroy]
+  before_action :set_current, only: [:show]
   before_action :authenticate_user!, except: [:index, :show]
 
   # GET /currents
@@ -13,64 +13,9 @@ class CurrentsController < ApplicationController
   def show
   end
 
-  # GET /currents/new
-  def new
-    @current = Current.new
-  end
-
-  # GET /currents/:id/edit
-  def edit
-  end
-
-  # POST /currents
-  # POST /currents.json
-  def create
-    @current = Current.new(current_params)
-    @current.owner = current_user
-
-    respond_to do |format|
-      if @current.save
-        format.html { redirect_to @current, notice: 'Current was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @current }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @current.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /currents/:id
-  # PATCH/PUT /currents/:id.json
-  def update
-    respond_to do |format|
-      if @current.update(current_params)
-        format.html { redirect_to @current, notice: 'Current was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @current.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /currents/:id
-  # DELETE /currents/:id.json
-  def destroy
-    @current.destroy
-    respond_to do |format|
-      format.html { redirect_to currents_url }
-      format.json { head :no_content }
-    end
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
+  # Use callbacks to share common setup or constraints between actions.
   def set_current
     @current = Current.find(params[:id])
-  end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-  def current_params
-    params.require(:current).permit(:title, :description)
   end
 end
