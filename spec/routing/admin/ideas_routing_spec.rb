0984bc1b8131b953bceaec338dde9decf42d6ebf
robinsonj <robinsonjosephlewis@gmail.com>
2014-05-14 09:36:29 -0700

    Update Admin Idea routing specs
      * Use 'expect...to' syntax.
      * Don't expect 'new' and 'show' to route to 'new/show' controller
        actions.

--- spec/routing/admin/ideas_routing_spec.rb
+++ spec/routing/admin/ideas_routing_spec.rb
@@ -4,31 +4,31 @@ describe Admin::IdeasController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/admin/ideas").should route_to("admin/ideas#index")
+      expect(get: '/admin/ideas').to route_to('admin/ideas#index')
     end
 
     it "routes to #new" do
-      get("/admin/ideas/new").should route_to("admin/ideas#new")
+      expect(get: '/admin/ideas/new').not_to route_to('admin/ideas#new')
     end
 
     it "routes to #show" do
-      get("/admin/ideas/1").should route_to("admin/ideas#show", :id => "1")
+      expect(get: '/admin/ideas/1').not_to route_to('admin/ideas#show', :id => '1')
     end
 
     it "routes to #edit" do
-      get("/admin/ideas/1/edit").should route_to("admin/ideas#edit", :id => "1")
+      expect(get: '/admin/ideas/1/edit').to route_to('admin/ideas#edit', :id => '1')
     end
 
     it "routes to #create" do
-      post("/admin/ideas").should route_to("admin/ideas#create")
+      expect(post: '/admin/ideas').to route_to('admin/ideas#create')
     end
 
     it "routes to #update" do
-      put("/admin/ideas/1").should route_to("admin/ideas#update", :id => "1")
+      expect(put: '/admin/ideas/1').to route_to('admin/ideas#update', :id => '1')
     end
 
     it "routes to #destroy" do
-      delete("/admin/ideas/1").should route_to("admin/ideas#destroy", :id => "1")
+      expect(delete: '/admin/ideas/1').to route_to('admin/ideas#destroy', :id => '1')
     end
 
   end
