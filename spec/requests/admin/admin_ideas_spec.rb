7fbd8c6714f9bb3db68c403b94c6faa1f8d98a04
robinsonj <robinsonjosephlewis@gmail.com>
2014-05-14 09:43:36 -0700

    Sign in an admin user before testing the request
    Before testing any requests to the admin interface, create and sign-in
    a user that has the 'admin' priveledge (the users table is empty at the
    start of the test; the first user created in an empty table gets the
    admin role).

--- spec/requests/admin/admin_ideas_spec.rb
+++ spec/requests/admin/admin_ideas_spec.rb
@@ -1,7 +1,18 @@
 require 'spec_helper'
 
 describe "Admin::Ideas" do
+
+  before(:all) do
+    @user = create(:user)
+    @user.confirm!
+  end
+
   describe "GET /admin_ideas" do
+
+    before do
+      post_via_redirect user_session_path, 'user[email]' => @user.email, 'user[password]' => @user.password
+    end
+
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       get admin_ideas_path
