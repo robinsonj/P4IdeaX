c0c10424da3e87a89bc68b322d55314d1b3f73c7
robinsonj <robinsonjosephlewis@gmail.com>
2014-05-14 09:07:29 -0700

    Remove Currents CRUD specs
    View rendering tests should not be performed for the CRUD views dealing
    with Currents. Remove them from the specs.

--- spec/views/currents_views_spec.rb
+++ spec/views/currents_views_spec.rb
@@ -10,19 +10,6 @@ describe 'CurrentsViews' do
     sign_in @user
   end
 
-  describe 'currents/edit' do
-    before(:each) do
-      @current = create(:current, owner_id: @user.id)
-    end
-
-    it 'renders the edit current form' do
-      render template: 'currents/edit'
-
-      assert_select 'form[action=?][method=?]', '/currents/' + @current.id.to_s, 'post' do
-      end
-    end
-  end
-
   describe 'currents/index' do
     before(:each) do
       assign(:currents, [
@@ -33,19 +20,6 @@ describe 'CurrentsViews' do
 
     it 'renders a list of currents' do
       render template: 'currents/index'
-    end
-  end
-
-  describe 'currents/new' do
-    before(:each) do
-      assign(:current, stub_model(Current).as_new_record)
-    end
-
-    it 'renders new current form' do
-      render template: 'currents/new'
-
-      assert_select 'form[action=?][method=?]', currents_path, 'post' do
-      end
     end
   end
 
