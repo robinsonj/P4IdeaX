5def9f3f1330fe17e4b0968024a95ae75e0a1180
robinsonj <robinsonjosephlewis@gmail.com>
2014-05-19 11:23:16 -0700

    Fix admin idea routing specs
    Use single quotes over double quotes when no string interpolation needs
    to occur.

--- spec/routing/admin/currents_routing_spec.rb
+++ spec/routing/admin/currents_routing_spec.rb
@@ -1,33 +1,33 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe Admin::CurrentsController do
-  describe "routing" do
+  describe 'routing' do
 
-    it "routes to #index" do
+    it 'routes to #index' do
       expect(get: '/admin/currents').to route_to('admin/currents#index')
     end
 
-    it "routes to #new" do
+    it 'routes to #new' do
       expect(get: '/admin/currents/new').not_to be_routable
     end
 
-    it "routes to #show" do
+    it 'routes to #show' do
       expect(get: '/admin/currents/1').not_to be_routable
     end
 
-    it "routes to #edit" do
+    it 'routes to #edit' do
       expect(get: '/admin/currents/1/edit').to route_to('admin/currents#edit', :id => '1')
     end
 
-    it "routes to #create" do
+    it 'routes to #create' do
       expect(post: '/admin/currents').to route_to('admin/currents#create')
     end
 
-    it "routes to #update" do
+    it 'routes to #update' do
       expect(put: '/admin/currents/1').to route_to('admin/currents#update', :id => '1')
     end
 
-    it "routes to #destroy" do
+    it 'routes to #destroy' do
       expect(delete: '/admin/currents/1').to route_to('admin/currents#destroy', :id => '1')
     end
 
--- spec/routing/admin/ideas_routing_spec.rb
+++ spec/routing/admin/ideas_routing_spec.rb
@@ -1,33 +1,33 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe Admin::IdeasController do
-  describe "routing" do
+  describe 'routing' do
 
-    it "routes to #index" do
+    it 'routes to #index' do
       expect(get: '/admin/ideas').to route_to('admin/ideas#index')
     end
 
-    it "routes to #new" do
+    it 'routes to #new' do
       expect(get: '/admin/ideas/new').not_to be_routable
     end
 
-    it "routes to #show" do
+    it 'routes to #show' do
       expect(get: '/admin/ideas/1').not_to be_routable
     end
 
-    it "routes to #edit" do
+    it 'routes to #edit' do
       expect(get: '/admin/ideas/1/edit').to route_to('admin/ideas#edit', :id => '1')
     end
 
-    it "routes to #create" do
+    it 'routes to #create' do
       expect(post: '/admin/ideas').to route_to('admin/ideas#create')
     end
 
-    it "routes to #update" do
+    it 'routes to #update' do
       expect(put: '/admin/ideas/1').to route_to('admin/ideas#update', :id => '1')
     end
 
-    it "routes to #destroy" do
+    it 'routes to #destroy' do
       expect(delete: '/admin/ideas/1').to route_to('admin/ideas#destroy', :id => '1')
     end
 
