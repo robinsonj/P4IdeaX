1970194dc4a491b59f224907af5b125fdc235544
robinsonj <robinsonjosephlewis@gmail.com>
2014-05-21 10:45:33 -0700

    Add a siebar-sublink to pending current Ideas
    Add a separate link to render only Ideas with pending currents and a
    bootstrap badge with the ideas count.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def currents_list
     Current.all.order(title: :desc).map{ |current| [current.title, current.id] }
   end
+
+  def pending_ideas_count
+    Idea.where(current_pending: true).count
+  end
 end
